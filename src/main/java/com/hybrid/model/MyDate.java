228b82222b6942e47d271a0909f60a32aca75ede
hsseo <protocols@nate.com>
2015-12-31 10:02:43 +0900

    xx

--- src/main/java/com/hybrid/model/MyDate.java
+++ src/main/java/com/hybrid/model/MyDate.java
@@ -1,9 +1,38 @@
 package com.hybrid.model;
 
+import java.io.IOException;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
+class MyDateSerializer extends JsonSerializer<Date> {
+
+	@Override
+	public void serialize(Date value, JsonGenerator gen, SerializerProvider serializers)
+			throws IOException, JsonProcessingException {
+		
+	}
+	
+}
+
+class MyDateDeSerializer extends JsonDeserializer<Date>{
+
+	@Override
+	public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+		return null;
+	}
+
+}
 
 public class MyDate {
 	int id;
